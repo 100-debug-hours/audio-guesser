2f945d93042cb04f4ee52a21e1d506066f6807ba
Arkadiy <arkadiy@ex.ua>
2020-01-23 02:29:44 +0200

    fix tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('audio-guesser-frontend');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('audio-guesser-frontend app is running!');
-  });
 });
