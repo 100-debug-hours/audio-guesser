7a66f3b099e8151fdc0efc5e8eb4d37b0f97ebac
Veetaha <gerzoh1@gmail.com>
2020-01-23 21:31:05 +0200

    chore(deployment): removed backend dist build

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PORT_DOCKER=3000
 PORT_LOCAL=8080
-PROJECT_NAME="audio-guesse"
+PROJECT_NAME="audio-guesser"
 docker build -t $PROJECT_NAME .
 docker run --env PORT=$PORT_DOCKER -p $PORT_LOCAL:$PORT_DOCKER -d $PROJECT_NAME
 echo "App is running on: 127.0.0.1:$PORT_LOCAL"
